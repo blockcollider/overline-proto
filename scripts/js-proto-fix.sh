2f814710fd289f1944163146582799d6347578bf
Adam Kloboucnik <adam@klob.cz>
2021-10-22 09:20:47 +0200

    Better impl/javascript structure
    - move generated protobufs one level deeper (impl/javascript/proto)
    - copy them to resulting npm package using pre* lifecycle scripts

--- scripts/js-proto-fix.sh
+++ scripts/js-proto-fix.sh
@@ -4,7 +4,7 @@ system=$(uname -s | tr '[:upper:]' '[:lower:]')
 regex='s/if (goog.DEBUG/if (true || goog.DEBUG/g'
 
 if [[ "$system" == 'darwin' ]]; then
-    sed -i '' "$regex" impl/javascript/*.js
+    sed -i '' "$regex" impl/javascript/proto/*.js
 elif [[ "$system" == 'linux' ]]; then
-    sed -i "$regex" impl/javascript/*.js
+    sed -i "$regex" impl/javascript/proto/*.js
 fi
